ec6d50a56eba51b796b10bd65a6070bb2a5d8d06
jarencudilla <jarencudilla@gmail.com>
2021-05-10 20:54:26 +0800

    Update classroom.sql

--- classroom.sql
+++ classroom.sql
@@ -4,57 +4,18 @@ create table classrooms (
     SECTION character varying(255),
 );
 
-INSERT INTO classrooms (id,student_id,SECTION) VALUES (
-  '1',
-  '1',
-  'A'
-);
-
-INSERT INTO classrooms (id,student_id,SECTION) VALUES (
-  '2',
-  '2',
-  'A'
-); 
-INSERT INTO classrooms (id,student_id,SECTION) VALUES (
-  '3',
-  '3',
-  'B'
-);
-INSERT INTO classrooms (id,student_id,SECTION) VALUES (
-  '4',
-  '4',
-  'C'
-); 
-INSERT INTO classrooms (id,student_id,SECTION) VALUES (
-  '5',
-  '5',
-  'B'
-); 
-INSERT INTO classrooms (id,student_id,SECTION) VALUES (
-  '6',
-  '6',
-  'A'
-); 
-INSERT INTO classrooms (id,student_id,SECTION) VALUES (
-  '7',
-  '7',
-  'C'
-); 
-INSERT INTO classrooms (id,student_id,SECTION) VALUES (
-  '8',
-  '8',
-  'B'
-); 
-INSERT INTO classrooms (id,student_id,SECTION) VALUES (
-  '9',
-  '9',
-  'B'
-); 
-INSERT INTO classrooms (id,student_id,SECTION) VALUES (
-  '10',
-  '10',
-  'C'
-); 
+INSERT INTO classrooms (id,student_id,SECTION) 
+VALUES 
+('1','1','A'),
+('2','2','A'), 
+('3','3','B'),
+('4','4','C'), 
+('5','5','B'),
+('6','6','A'), 
+('7','7','C'), 
+('8','8','B'), 
+('9','9','B'), 
+('10','10','C'); 
 
 -- inner join
 SELECT * FROM students s INNER JOIN classrooms c ON s.id = c.student_id;
